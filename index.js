32be89cac5acaffb39443dc4c7c23fcd4166a1b5
Harshit singh sengar <sengarharshit15@gmail.com>
2023-10-10 21:10:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("#forms");
+const email = document.querySelector("#email");
+
+
+
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let emailValue =email.value;
+   
+
+    if (validateEmail(emailValue)) {
+        form.classList.add("message");
+        form.classList.remove("error");
+
+    } else {
+        form.classList.add("error")
+    }
+} )
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
